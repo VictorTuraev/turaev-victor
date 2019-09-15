efc699cf52a1d6c04f01e3d2a21e70287437d45e
VictorTuraev <55141182+VictorTuraev@users.noreply.github.com>
2019-09-15 21:05:47 +0300

    Update task_15.py

--- turaev/task_15.py
+++ turaev/task_15.py
@@ -6,22 +6,22 @@ from pyrob.api import *
 @task
 def task_8_21():
     while True:
-
         if wall_is_on_the_right() and (wall_is_above():
             move_down(n=9)
             move_left(n=9)
-           
+            break
         if wall_is_on_the_left() and wall_is_above():
             move_right(n=9)
             move_down(n=9)
-            
+            break
         if wall_is_on_the_left() and wall_is_beneath():
             move_up(n=9)
             move_right(n=9)
-            
+            break
         if wall_is_on_the_right() and wall_is_beneath():
             move_up(n=9)
             move_left(n=9)
+            break
             
 if __name__ == '__main__':
     run_tasks()
