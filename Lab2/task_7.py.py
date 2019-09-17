df73b3af5fcf57a691155ff0f45ca053c1341478
VictorTuraev <55141182+VictorTuraev@users.noreply.github.com>
2019-09-17 21:29:14 +0300

    Update task_7.py.py

--- Lab2/task_7.py.py
+++ Lab2/task_7.py.py
@@ -1,18 +1,12 @@
 import turtle
 import math
-turtle.speed(0)
-r=3
-d=1
-turtle.forward(r)
-n=100000000
-s=2*math.pi/360
+f=0
+k=10
+n=1000
 for i in range(n):
-    l=r**2+(r+d)**2-2*r*(r+d)*math.cos(s)
-    c=180/math.pi*math.acos((r**2+l**2-(r+d)**2)/(2*r*l))
-    turtle.left(c)
-    r=r+d
-    turtle.forward(l)
-    turtle.right(c-s*180/math.pi)
+    goto(k*f*math.cos(f),k*f*math.sin(f))
+    f=f+1
+
     
     
     
