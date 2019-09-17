684cc584b034a7ff378fda147e7e41b86fdab06c
VictorTuraev <55141182+VictorTuraev@users.noreply.github.com>
2019-09-17 20:31:53 +0300

    Update task_10.py

--- Lab2/task_10.py
+++ Lab2/task_10.py
@@ -2,14 +2,11 @@ import turtle
 import math
 r=50
 n=3
-turtle.right(90)
 for i in range(n):
     turtle.circle(r)
-    turtle.right(0.5)
-    for i in range(360):
-        turtle.forward(r*math.sin(math.pi/180))
-        turtle.right(1)
-    turtle.right(0.5)
+    turtle.right(180)
+    turtle.circle(r)
+    turtle.right(180)
     turtle.right(45)
     
     
