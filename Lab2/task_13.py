065261012e3fb857116abead33e050088bddac33
VictorTuraev <55141182+VictorTuraev@users.noreply.github.com>
2019-09-19 20:17:45 +0300

    Update task_13.py

--- Lab2/task_13.py
+++ Lab2/task_13.py
@@ -2,31 +2,32 @@ import turtle
 import math
 turtle.speed(0)
 turtle.left(90)
-def face():
-    turtle.circle(100)
-def eyes():
-    turtle.penup()
-    turtle.goto(-45,50)
-    turtle.pendown()
-    turtle.circle(5)
-    turtle.penup()
-    turtle.goto(-145,50)
-    turtle.pendown()
-    turtle.circle(5)
-def nose():
-    turtle.penup()
-    turtle.goto(-100,-10)
-    turtle.pendown()
-    turtle.forward(20)
-    turtle.penup()
-def smile():
-    turtle.goto(-75,-50)
-    turtle.right(180)
-    r=25
-    turtle.pendown()
-    for i in range(180):
-        turtle.forward(r*math.sin(math.pi/180))
-        turtle.right(1)
+
+turtle.circle(100)
+
+turtle.penup()
+turtle.goto(-45,50)
+turtle.pendown()
+turtle.circle(5)
+turtle.penup()
+turtle.goto(-145,50)
+turtle.pendown()
+turtle.circle(5)
+
+turtle.penup()
+turtle.goto(-100,-10)
+turtle.pendown()
+turtle.forward(20)
+turtle.penup()
+
+turtle.goto(-75,-50)
+turtle.right(180)
+r=25
+turtle.pendown()
+for i in range(180):
+    turtle.forward(r*math.sin(math.pi/180))
+    turtle.right(1)
+
 
     
         
