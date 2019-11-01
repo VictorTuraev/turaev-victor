f6372268f44bae398371a05bbb8ce070c54e5b1a
turaev-victor <turaev.va@phystech.edu>
2019-11-01 18:21:37 +0300

    Исправил маятник

--- lab3_3/image.py
+++ lab3_3/image.py
@@ -3,7 +3,7 @@ import math
 win = gr.GraphWin("mayatnik", 500, 500)
 dt = 10
 a = 0
-v = 500
+v = 0.01
 r = 20
 l=200
 x=250
@@ -15,43 +15,36 @@ circle = gr.Circle(gr.Point(x, y), r)
 circle.setFill("black")
 circle.draw(win)
 
-def may():
-	global x, y, circle
-	x, y = mv(a)
-	circle = gr.Circle(gr.Point(x, y), r)
-	circle.setFill("black")
-	circle.draw(win)
 
-def change(a, v):
-	k=v*dt/r
-	b=-10*math.sin(a)
-	a=a+k
-	v=v+b
+def change():
+	global a, v
+	a=a+v*dt/r
+	v=v-0.01*math.sin(a)
 	return a
-	
-def nt():
-	global x, y, nit
-	x, y=mv(a)
-	nit = gr.Line(gr.Point(250, 10), gr.Point(x, y))
-	nit.draw(win)
-	gr.time.sleep(0.2)
+
 	
 def mv(a):
-	global nit, circle
-	a = change(a, v)
+	global nit, circle, x, y, l
+	a = change()
 	x=l*math.sin(a)+250
 	y=l*math.cos(a)+10
 	nit.undraw()
-	gr.time.sleep(0.2)
+	circle.undraw()
 	circle.move(x, y)
-	return x, y
+	
+	nit = gr.Line(gr.Point(250, 10), gr.Point(x, y))
+	nit.draw(win)
+	
+	
+	circle = gr.Circle(gr.Point(x, y), r)
+	circle.setFill("black")
+	circle.draw(win)
+	
 def main():
 	for i in range(500):
-		may()
-		nt()
-		change(a, v)
+		change()
 		mv(a)
-		gr.time.sleep(0.2)
+		gr.time.sleep(0.1)
 main()
 
 
