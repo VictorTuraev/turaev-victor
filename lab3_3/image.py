e3ff18b4a9b8f19105381656c36384990119adcc
turaev-victor <turaev.va@phystech.edu>
2019-11-04 21:29:14 +0300

    flake8

--- lab3_3/image.py
+++ lab3_3/image.py
@@ -5,9 +5,9 @@ dt = 10
 a = 0
 v = 0.05
 r = 20
-l=200
-x=250
-y=10+l*math.cos(a)
+l = 200
+x = 250
+y = 10+l*math.cos(a)
 
 nit = gr.Line(gr.Point(250, 10), gr.Point(x, y))
 nit.draw(win)
@@ -18,16 +18,16 @@ circle.draw(win)
 
 def change():
 	global a, v
-	a=a+v*dt/r
-	v=v-0.01*math.sin(a)
+	a = a+v*dt/r
+	v = v-0.01*math.sin(a)
 	return a
 
 	
 def mv():
 	global nit, circle, x, y, l
 	a = change()
-	x=l*math.sin(a)+250
-	y=l*math.cos(a)+10
+	x = l*math.sin(a)+250
+	y = l*math.cos(a)+10
 	nit.undraw()
 	circle.undraw()
 	circle.move(x, y)
