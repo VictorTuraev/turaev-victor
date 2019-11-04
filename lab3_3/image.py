08d93f51eff02b517325099447af451abe66a528
turaev-victor <turaev.va@phystech.edu>
2019-11-04 21:38:57 +0300

    flake8

--- lab3_3/image.py
+++ lab3_3/image.py
@@ -8,7 +8,6 @@ r = 20
 l = 200
 x = 250
 y = 10+l*math.cos(a)
-
 nit = gr.Line(gr.Point(250, 10), gr.Point(x, y))
 nit.draw(win)
 circle = gr.Circle(gr.Point(x, y), r)
@@ -17,34 +16,32 @@ circle.draw(win)
 
 
 def change():
-	global a, v
-	a = a+v*dt/r
-	v = v-0.01*math.sin(a)
-	return a
+    global a, v
+    a = a+v*dt/r
+    v = v-0.01*math.sin(a)
+    return a
 
-	
+
 def mv():
-	global nit, circle, x, y, l
-	a = change()
-	x = l*math.sin(a)+250
-	y = l*math.cos(a)+10
-	nit.undraw()
-	circle.undraw()
-	circle.move(x, y)
-	
-	nit = gr.Line(gr.Point(250, 10), gr.Point(x, y))
-	nit.draw(win)
-	
-	
-	circle = gr.Circle(gr.Point(x, y), r)
-	circle.setFill("black")
-	circle.draw(win)
-	
+    global nit, circle, x, y, l
+    a = change()
+    x = l*math.sin(a)+250
+    y = l*math.cos(a)+10
+    nit.undraw()
+    circle.undraw()
+    circle.move(x, y)
+    nit = gr.Line(gr.Point(250, 10), gr.Point(x, y))
+    nit.draw(win)
+    circle = gr.Circle(gr.Point(x, y), r)
+    circle.setFill("black")
+    circle.draw(win)
+
+
 def main():
-	for i in range(500):
-		change()
-		mv()
-		gr.time.sleep(0.1)
+    for i in range(500):
+        change()
+        mv()
+        gr.time.sleep(0.1)
+
+
 main()
-
-
